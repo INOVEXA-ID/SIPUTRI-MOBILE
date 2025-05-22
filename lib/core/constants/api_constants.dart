4288c384f8b71b1903adf0762e16ad276a00c348
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-05-22 18:50:13 +0700

    add dotenv

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,1 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+class ApiConstants {
+  static String? baseUrl = dotenv.env['HOST'];
+  static String? baseUrlImage = dotenv.env['URL'];
+
+  static String loginEndpoint = '$baseUrl/login-mahasiswa';
+}
