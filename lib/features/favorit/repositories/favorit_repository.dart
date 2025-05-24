d924597a2f43d3b5bc1059d241def0dd6cbe1592
Muhammad Reza Ivano Pahlevi <rezaivano123@gmail.com>
2025-05-24 23:50:41 +0700

    Perbaikan UI Card Hasil Search

--- lib/features/favorit/repositories/favorit_repository.dart
+++ lib/features/favorit/repositories/favorit_repository.dart
@@ -1,6 +1,7 @@
 import 'package:siputri_mobile/core/constants/api_constants.dart';
 import 'package:siputri_mobile/core/services/dio_client.dart';
 import 'package:siputri_mobile/features/favorit/models/favorit_model.dart';
+import 'package:siputri_mobile/core/helper/token_storage.dart'; // Tambahkan ini
 
 class FavoritRepository {
   final DioClient dioClient;
@@ -8,8 +9,15 @@ class FavoritRepository {
   FavoritRepository({required this.dioClient});
 
   Future<FavoritModel> getFavorit() async {
+    // Ambil id_user dari token
+    final user = TokenStorage().user;
+    if (user == null) {
+      throw Exception('User belum login!');
+    }
+    final idUser = user.idUser;
+
     final response = await dioClient.dio.get(
-      "${ApiConstants.favoritEndpoint}/2",
+      "${ApiConstants.favoritEndpoint}/$idUser",
     );
     if (response.statusCode == 200) {
       final data = response.data;
